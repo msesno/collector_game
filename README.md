4c8440363c5a6c4e52940a55271aaa236a30afa6
msesno <msesno@gmail.com>
2019-04-02 23:23:16 -0400

    rearrange file struct & put working files

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# collector_game
-A poke collection game
+
+# Poke Collector
+<!-- Javascript jQuery game -->
+
+
+# Pseudo Code:
+<!-- There are four pokemon displayed as buttons on the page. -->
+
+<!-- The player will be shown a random number at the start of the game. -->
+
+<!-- When the player clicks on a pokemon, it will add a specific amount of points to the player's total score.  -->
+
+<!-- The game will hide this amount until the player clicks a poke. -->
+
+<!-- When they click one, the player score updates. -->
+
+<!-- The poke is caught if their total score matches the random number provided at the beginning. -->
+
+<!-- The poke flees if their score goes above the random number. -->
+
+<!-- The game restarts whenever the player wins or loses. -->
+
+<!-- When the game begins again, the player will see a new random number, and all the pokes will have four new hidden values. Of course, the user's score (and score counter) will reset to zero. -->
+
+<!-- The score tracks the number of player catches and flees. -->
---
+++ assets/javascript/script.js
@@ -0,0 +1,101 @@
+// Pokemon Catch Game
+
+// beginning score rng
+var targetNumber = Math.floor(Math.random() * (50) + 70);
+
+$("#targetScore").html("Target Score: " + targetNumber);
+
+// scoreboard variables 
+var wins = 0;
+var losses = 0;
+var counter = 0;
+
+// funtion for reroll 4 numbers - CANNOT GET WORKING
+// function reroll (){
+// 	var i = clearInterval(fourNumbers);
+// 	for (var i=0; i<4; i++) {	
+// 		fourNumbers.push(numberOptions[Math.floor(Math.random() * numberOptions.length)]);
+// 	}
+// }
+
+// assign rng values 1-12 to the four pokes
+var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+
+// numbers array
+var fourNumbers = [];
+
+
+// loop for 4 random numbers - can not guarantee four DIFFERENT numbers
+for (var i=0; i<4; i++) {	
+	fourNumbers.push(numberOptions[Math.floor(Math.random() * numberOptions.length)]);
+}
+
+// add 4 images to the page and assign fourNumbers
+	var bulba = $("<img>");
+	bulba.addClass("poke-image");
+	bulba.attr("src", "assets/images/bulbasaur.gif");
+	bulba.attr("data-pokevalue", fourNumbers[0]);
+	$("#pokes").append(bulba);
+
+	var squirt = $("<img>");
+	squirt.addClass("poke-image");
+	squirt.attr("src", "assets/images/squirtle.gif");
+	squirt.attr("data-pokevalue", fourNumbers[1]);
+	$("#pokes").append(squirt);
+
+	var pika = $("<img>");
+	pika.addClass("poke-image");
+	pika.attr("src", "assets/images/pikachu.gif");
+	pika.attr("data-pokevalue", fourNumbers[2]);
+	$("#pokes").append(pika);
+
+	var char = $("<img>");
+	char.addClass("poke-image");
+	char.attr("src", "assets/images/charmander.gif");
+	char.attr("data-pokevalue", fourNumbers[3]);
+	$("#pokes").append(char);
+
+
+
+// on-click event for image class "poke-image"
+$(".poke-image").on("click", function() {
+	console.log("poke values: " + fourNumbers);
+
+	var pokeValue = ($(this).attr("data-pokevalue"));
+	pokeValue = parseInt(pokeValue);
+
+	counter += pokeValue;
+	$("#currentScore").html("Current Score: " + counter);
+
+	// check if click counter matches targetNumber
+	if (counter === targetNumber) {
+		wins++;
+		$("#winlose").html("POKEMON CAUGHT!");
+		counter = 0;
+		targetNumber = Math.floor(Math.random() * (50)) + 70;
+		$("#targetScore").html("Target Score: " + targetNumber);
+		$("#currentScore").html("Current Score: 0 ");
+		// reroll();
+		// console.log("poke values: " + fourNumbers);
+		// TODO: can't get it to re-select fourNumbers
+		// TODO: clear the "you win"/"you lose" on new iteration of the game
+	}
+	// if click counter exceeds targetNumber
+	else if (counter >= targetNumber) {
+		$("#winlose").html("POKEMON FLED!");
+		losses++;
+		counter = 0;
+		targetNumber = Math.floor(Math.random() * (50)) + 70;
+		$("#targetScore").html("Target Score: " + targetNumber);
+		$("#currentScore").html("Current Score: 0 ");
+		// reroll();
+		// console.log("poke values: " + fourNumbers);
+		// TODO: can't get it to re-select fourNumbers
+		// TODO: clear the "you win"/"you lose" on new iteration of the game
+	}
+
+	// update scoreboard with wins and losses
+	$("#scoreboard").html("<p>Caught: " + wins + "</p>" + "<p>Fled: " + losses + "</p>")
+
+});
